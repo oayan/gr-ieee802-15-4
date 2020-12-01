205ad24dc3a394deab1b84015b255fdec54931ce
hyagizozkan <hasanyagiz.ozkan@gmail.com>
2020-12-01 11:31:22 +0100

    never using git=?

---
+++ inverted_pendulum_gui/examples/top_block.py
@@ -0,0 +1,199 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# GNU Radio version: 3.7.14.0
+##################################################
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+import os
+import sys
+sys.path.append(os.environ.get('GRC_HIER_PATH', os.path.expanduser('~/.grc_gnuradio')))
+
+from PyQt4 import Qt
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio import uhd
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from gnuradio.qtgui import Range, RangeWidget
+from ieee802_15_4_oqpsk_phy import ieee802_15_4_oqpsk_phy  # grc-generated hier_block
+from optparse import OptionParser
+import ieee802_15_4
+import time
+from gnuradio import qtgui
+
+
+class top_block(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.restoreGeometry(self.settings.value("geometry").toByteArray())
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.tx_gain = tx_gain = 0.75
+        self.rx_gain = rx_gain = 0.75
+        self.freq_controller_tx = freq_controller_tx = 2610000000
+        self.freq_controller_rx = freq_controller_rx = 2575000000
+
+        self.channel_filter = channel_filter = firdes.complex_band_pass(1.0, 384000, -80000, 80000, 20000, firdes.WIN_HAMMING, 6.76)
+
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self._tx_gain_range = Range(0, 1, 0.01, 0.75, 200)
+        self._tx_gain_win = RangeWidget(self._tx_gain_range, self.set_tx_gain, "tx_gain", "counter_slider", float)
+        self.top_grid_layout.addWidget(self._tx_gain_win)
+        self._rx_gain_range = Range(0, 1, 0.01, 0.75, 200)
+        self._rx_gain_win = RangeWidget(self._rx_gain_range, self.set_rx_gain, "rx_gain", "counter_slider", float)
+        self.top_grid_layout.addWidget(self._rx_gain_win)
+        self.uhd_usrp_source_0 = uhd.usrp_source(
+        	",".join(('', "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        )
+        self.uhd_usrp_source_0.set_samp_rate(4000000)
+        self.uhd_usrp_source_0.set_center_freq(freq_controller_rx, 0)
+        self.uhd_usrp_source_0.set_normalized_gain(rx_gain, 0)
+        self.uhd_usrp_source_0.set_auto_dc_offset(False, 0)
+        self.uhd_usrp_source_0.set_auto_iq_balance(False, 0)
+        self.uhd_usrp_sink_0 = uhd.usrp_sink(
+        	",".join(('', "")),
+        	uhd.stream_args(
+        		cpu_format="fc32",
+        		channels=range(1),
+        	),
+        	'pdu_length',
+        )
+        self.uhd_usrp_sink_0.set_samp_rate(4000000)
+        self.uhd_usrp_sink_0.set_center_freq(freq_controller_rx, 0)
+        self.uhd_usrp_sink_0.set_normalized_gain(tx_gain, 0)
+        self.ieee802_15_4_rime_stack_0 = ieee802_15_4.rime_stack(([129]), ([131]), ([132]), ([23,42]))
+        self.ieee802_15_4_oqpsk_phy_0 = ieee802_15_4_oqpsk_phy()
+        self.ieee802_15_4_mac_controller_0 = ieee802_15_4.mac_controller(False,0x8841,0,0x1aaa,10,3,10,False,0,0)
+        self.ieee802_15_4_csma_0 = ieee802_15_4.csma()
+        self.fft_filter_xxx_0 = filter.fft_filter_ccc(1, (channel_filter), 1)
+        self.fft_filter_xxx_0.declare_sample_delay(0)
+        self.blocks_socket_pdu_0_0_0 = blocks.socket_pdu("UDP_CLIENT", '127.0.0.1', '5050', 10000, False)
+        self.blocks_socket_pdu_0_0 = blocks.socket_pdu("UDP_SERVER", '127.0.0.1', '5000', 10000, False)
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.msg_connect((self.blocks_socket_pdu_0_0, 'pdus'), (self.ieee802_15_4_rime_stack_0, 'bcin'))
+        self.msg_connect((self.ieee802_15_4_csma_0, 'out'), (self.ieee802_15_4_mac_controller_0, 'cca in'))
+        self.msg_connect((self.ieee802_15_4_mac_controller_0, 'cca out'), (self.ieee802_15_4_csma_0, 'mac in'))
+        self.msg_connect((self.ieee802_15_4_mac_controller_0, 'pdu out'), (self.ieee802_15_4_oqpsk_phy_0, 'txin'))
+        self.msg_connect((self.ieee802_15_4_mac_controller_0, 'app out'), (self.ieee802_15_4_rime_stack_0, 'fromMAC'))
+        self.msg_connect((self.ieee802_15_4_oqpsk_phy_0, 'rxout'), (self.ieee802_15_4_mac_controller_0, 'pdu in'))
+        self.msg_connect((self.ieee802_15_4_rime_stack_0, 'bcout'), (self.blocks_socket_pdu_0_0_0, 'pdus'))
+        self.msg_connect((self.ieee802_15_4_rime_stack_0, 'toMAC'), (self.ieee802_15_4_mac_controller_0, 'app in'))
+        self.connect((self.fft_filter_xxx_0, 0), (self.ieee802_15_4_csma_0, 0))
+        self.connect((self.ieee802_15_4_oqpsk_phy_0, 0), (self.uhd_usrp_sink_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.fft_filter_xxx_0, 0))
+        self.connect((self.uhd_usrp_source_0, 0), (self.ieee802_15_4_oqpsk_phy_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_tx_gain(self):
+        return self.tx_gain
+
+    def set_tx_gain(self, tx_gain):
+        self.tx_gain = tx_gain
+        self.uhd_usrp_sink_0.set_normalized_gain(self.tx_gain, 0)
+
+
+    def get_rx_gain(self):
+        return self.rx_gain
+
+    def set_rx_gain(self, rx_gain):
+        self.rx_gain = rx_gain
+        self.uhd_usrp_source_0.set_normalized_gain(self.rx_gain, 0)
+
+
+    def get_freq_controller_tx(self):
+        return self.freq_controller_tx
+
+    def set_freq_controller_tx(self, freq_controller_tx):
+        self.freq_controller_tx = freq_controller_tx
+
+    def get_freq_controller_rx(self):
+        return self.freq_controller_rx
+
+    def set_freq_controller_rx(self, freq_controller_rx):
+        self.freq_controller_rx = freq_controller_rx
+        self.uhd_usrp_source_0.set_center_freq(self.freq_controller_rx, 0)
+        self.uhd_usrp_sink_0.set_center_freq(self.freq_controller_rx, 0)
+
+    def get_channel_filter(self):
+        return self.channel_filter
+
+    def set_channel_filter(self, channel_filter):
+        self.channel_filter = channel_filter
+        self.fft_filter_xxx_0.set_taps((self.channel_filter))
+
+
+def main(top_block_cls=top_block, options=None):
+
+    from distutils.version import StrictVersion
+    if StrictVersion(Qt.qVersion()) >= StrictVersion("4.5.0"):
+        style = gr.prefs().get_string('qtgui', 'style', 'raster')
+        Qt.QApplication.setGraphicsSystem(style)
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.connect(qapp, Qt.SIGNAL("aboutToQuit()"), quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
